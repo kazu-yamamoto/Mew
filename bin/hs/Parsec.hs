a709d35d41976dfe9977761c653833cf1a1a6c19
Kazu Yamamoto <kazu@iij.ad.jp>
2010-05-11 11:18:58 +0900

    large ifdef.

--- bin/hs/Parsec.hs
+++ bin/hs/Parsec.hs
@@ -1,24 +1,25 @@
 {-# LANGUAGE CPP #-}
 
-module Parsec (
 #if MIN_VERSION_parsec(3,0,0)
+module Parsec (
       module Control.Applicative
     , module Text.Parsec
     , module Text.Parsec.String
-#else
-      module Control.Applicative
-    , module Text.ParserCombinators.Parsec
-#endif
     ) where
 
 import Control.Applicative
-
-#if MIN_VERSION_parsec(3,0,0)
 import Text.Parsec
 import Text.Parsec.String
 #else
+module Parsec (
+      module Control.Applicative
+    , module Text.ParserCombinators.Parsec
+    ) where
+
+import Control.Applicative
 import Control.Monad (MonadPlus(..), ap)
 import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
+
 instance Applicative (GenParser s a) where
     pure  = return
     (<*>) = ap
