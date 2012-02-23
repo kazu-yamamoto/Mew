1f4036f6d18733f3a4a032cc4ee72f3b1ba6121d
Kazu Yamamoto <kazu@iij.ad.jp>
2012-02-23 12:16:21 +0900

    highlighting html produced by w3m.

--- mew-mime.el
+++ mew-mime.el
@@ -256,7 +256,9 @@
 					mew-use-text/html-list
 					mew-use-text/html-string-type
 					mew-use-text/html-list-type)))
-	      (funcall mew-prog-text/html start (point-max))
+	      (progn
+		(funcall mew-prog-text/html start (point-max))
+		(mew-highlight-body-region start (point-max)))
 	    (mew-message-for-summary "To parse HTML, type '\\[mew-summary-analyze-again]'"))))
     (insert " #     # ####### #     # #\n"
 	    " #     #    #    ##   ## #\n"
