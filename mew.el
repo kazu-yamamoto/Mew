516b2697e08b052352949ca97c0cecf719c18443
Kazu Yamamoto <kazu@iij.ad.jp>
2015-05-13 15:36:00 +0900

    ver bumps up.

--- mew.el
+++ mew.el
@@ -2,7 +2,7 @@
 
 ;; Author:  Kazu Yamamoto <Kazu@Mew.org>
 ;; Created: Mar 23, 1994
-;; Revised: Apr 29, 2014
+;; Revised: May 13, 2015
 
 ;;; Commentary:
 
@@ -18,7 +18,7 @@
 ;;; Mew version
 ;;;
 
-(defconst mew-version-number "6.6"
+(defconst mew-version-number "6.7rc1"
   "Version number for this version of Mew.")
 (defconst mew-version (format "Mew version %s" mew-version-number)
   "Version string for this version of Mew.")
