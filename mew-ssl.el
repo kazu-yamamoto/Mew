97ad84ef9cbb984817f5e7e99bcdb89782774772
Tatsuya Kinoshita <tats@debian.org>
2018-05-26 00:45:26 +0900

    Don't specify obsolete sslVersion

--- mew-ssl.el
+++ mew-ssl.el
@@ -118,7 +118,7 @@ insert no extra text.")
 	(insert (format "[%d]\n" localport))
 	(insert (format "accept=%s:%d\n" mew-ssl-localhost localport))
 	(insert (format "connect=%s:%d\n" server remoteport))
-	(if tls (insert (format "protocol=%s\nsslVersion=TLSv1\n" tls)))
+	(if tls (insert (format "protocol=%s\n" tls)))
 	(mew-frwlet mew-cs-dummy mew-cs-text-for-write
 	  ;; NEVER use call-process-region for privacy reasons
 	  (write-region (point-min) (point-max) file nil 'no-msg))
