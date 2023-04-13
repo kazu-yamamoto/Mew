b48a806bb16b1f9dc9ff14b06bde95c8f393d6b3
Kazu Yamamoto <kazu@iij.ad.jp>
2023-04-13 14:39:55 +0900

    placing mew-mode-line-format in the end of mode-line-format.

--- elisp/mew-summary.el
+++ elisp/mew-summary.el
@@ -261,8 +261,6 @@ and return (beg . end)."
 (defvar mew-mode-line-target "%p")
 (defvar mew-mode-line-format
   `(""
-    (mew-summary-buffer-left-msgs mew-summary-buffer-left-msgs "L%l")
-    (mew-summary-buffer-raw "*")
     (mew-summary-buffer-secure-process ,mew-secure-format2)))
 
 (defvar mew-mode-line-process
@@ -272,20 +270,11 @@ and return (beg . end)."
 (defun mew-summary-setup-mode-line ()
   (let ((tgt mew-mode-line-target)
 	target prev pos)
-    (if (boundp 'mode-line-position)
-	(progn
-	  (make-local-variable 'mode-line-position) ;; Emacs 21.3.50
-	  (setq mode-line-position
-		(copy-sequence (default-value 'mode-line-position)))
-	  (setq prev mode-line-position))
-      (setq mode-line-format (copy-sequence (default-value 'mode-line-format)))
-      (setq prev mode-line-format))
-    (setq target (or (rassoc (list tgt) prev) ;; Emacs 21.3.50
-		     (rassoc tgt prev)
-		     (car (member tgt prev))))
-    (when target
-      (setq pos (- (length prev) (length (member target prev))))
-      (setcar (nthcdr pos prev) mew-mode-line-format))
+    (when (boundp 'mode-line-format)
+      (make-local-variable 'mode-line-format)
+      (setq mode-line-format
+	    (copy-sequence (default-value 'mode-line-format)))
+      (nconc mode-line-format mew-mode-line-format))
     (when (boundp 'line-number-mode)
       (make-local-variable 'line-number-mode)
       (setq line-number-mode nil))
