fc62d6c43d4c5e583fd5e9904b6311b3a77a8f44
nbkenichi <154867291+nbkenichi@users.noreply.github.com>
2025-10-23 20:25:17 +0900

    fix: change a method for hiding BIMI-Indicator header to keep a blank line before a mail body

--- elisp/mew-highlight.el
+++ elisp/mew-highlight.el
@@ -285,16 +285,14 @@
     (goto-char beg)
     (mew-elet
      (let ((regex1 "^BIMI-Indicator: *\\(.*\\)\n")
-	   (wmsg " No 'bimi=pass' in Authentication-Results\n")
-	   overlay bimi-logo bimi-pass svgb64 beg0 end0 scale)
+	   (wmsg " No 'bimi=pass' in Authentication-Results: regarding BIMI-Indicator:\n")
+	   overlay bimi-logo bimi-pass svgb64 scale)
        (setq bimi-pass (string-match "\\bbimi=pass\\b" (or (mew-header-get-value "Authentication-Results:") "")))
        (when (re-search-forward regex1 end t)
-	 (setq beg0 (match-beginning 0))
-	 (setq end0 (match-end 0))
 	 (setq svgb64 (match-string-no-properties 1))
 	 (cond
 	  ((and mew-use-bimi-status-check (not bimi-pass))
-	   (goto-char beg0)
+	   (goto-char end)
 	   (insert mew-x-mew:)
 	   (setq overlay (mew-overlay-make (- (point) (length mew-x-mew:)) (point)))
 	   (overlay-put overlay 'face 'mew-face-header-important)
@@ -305,13 +303,10 @@
 	   (when (image-type-available-p 'svg)
 	     (if (boundp 'text-scale-mode)
 		 (setq scale (expt text-scale-mode-step text-scale-mode-amount))
-	       (setq scale 1.0)
-	       )
+	       (setq scale 1.0))
 	     (setq bimi-logo (create-image (mew-base64-decode-string svgb64) 'svg t
 					   :width (round (* mew-bimi-logo-size scale)) :ascent 'center)))
 	   (when bimi-logo
-	     (setq overlay (mew-overlay-make beg0 end0))
-	     (overlay-put overlay 'invisible t)
 	     (save-restriction
 	       (narrow-to-region beg end)
 	       (mew-bimi-display bimi-logo))))))))))
